9e5a9713bd67af1b3c39dab209a6045ce4b92d08
quang <nguyenthequang080203@gmail.com>
2023-11-03 08:47:50 +0700

    add shortener component ui

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Advanced from "./components/Advanced";
 import Boost from "./components/Boost";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Shortener from "./components/Shortener";
+import Showcase from "./components/Showcase";
 
 function App() {
   return (
     <div className="w-screen h-screen">
       <div className="w-full h-full">
         <Header />
+        <Showcase />
+        <Shortener/>
         <Advanced/>
         <Boost />
         <Footer />
--- src/components/Shortener.js
+++ src/components/Shortener.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState } from "react"
+import bgMobile from "../images/bg-shorten-mobile.svg"
+import bgDesktop from "../images/bg-shorten-desktop.svg"
 
 function Shortener() {
-  return (
-    <div>Shortener</div>
-  )
+    const [text, setText] = useState("")
+
+    const handleSubmit = (e) => {}
+
+    return (
+        <>
+            <section className="max-width shortener relative">
+                <picture>
+                    <source media="(min-width: 768px)" srcSet={bgDesktop} />
+                    <img src={bgMobile} alt="" />
+                </picture>
+
+                <form className="form" onSubmit={handleSubmit}>
+                    <div className="flex flex-col md:flex-row">
+                        <input
+                            type="url"
+                            placeholder="Shorten a link here"
+                            className="w-full py-2 px-5 rounded-lg mb-2 md:mb-0 md:w-2/3"
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                        />
+                        <button
+                            type="submit"
+                            className="btn-cta rounded-lg w-full md:w-40 md:ml-5"
+                            onClick={handleSubmit}
+                        >
+                            Shorten It!
+                        </button>
+                    </div>
+                </form>       
+            </section>
+        </>
+    )
 }
 
 export default Shortener
