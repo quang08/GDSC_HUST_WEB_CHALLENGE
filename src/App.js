420f41bcef1e78e9c96c5efe46403f980ba5ac6d
quang <nguyenthequang080203@gmail.com>
2023-11-03 08:05:14 +0700

    add boost component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import Boost from "./components/Boost";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Footer />
+    <div className="w-screen h-screen">
+      <div className="w-full h-full">
+
+
+        <Header />
+        <Boost />
+        <Footer />
+      </div>
     </div>
   );
 }
